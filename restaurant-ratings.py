1a012740e94a82e673bd7397de6a93dad150c832
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 17:50:30 -0800

    allowed user to input new restaurant and score

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -2,6 +2,16 @@ import sys
 
 # file_name = sys.argv[1]
 
+restaurant_score = {}
+
+def print_restaurant_score(restaurant_dict):
+
+    restaurant_keys = sorted(restaurant_dict)
+
+    for res in restaurant_keys:
+        print "{} is rated at {}.".format(res, restaurant_score[res])
+
+
 def rate_restaurant():
     """Print out restaurants with its rating.
 
@@ -16,23 +26,31 @@ def rate_restaurant():
 
     data_file = open(sys.argv[1])
 
-    restaurant_score = {}
 
     for line in data_file:
         line = line.rstrip()
         res_rating = line.split(":")
-        key = res_rating[0]
-        value = res_rating[1]
-        restaurant_score[key] = value
+        restaurant = res_rating[0]
+        score = int(res_rating[1])
+        restaurant_score[restaurant] = score
 
 
-    restaurant_keys = sorted(restaurant_score)
-
-    for res in restaurant_keys:
-        print res, "is rated at", restaurant_score[res] + '.'
+    print_restaurant_score(restaurant_score)
 
 
     data_file.close()
 
 rate_restaurant()
+
+choice = raw_input("Do you want to add a new restaurant? Y/N")
+
+if choice[0] == 'Y' or choice[0] == 'y':
+    new_restaurant = raw_input("Enter the restaurant name. ")
+    new_score = int(raw_input('Rate this restaurant on a scale of 0 - 5. '))
+    restaurant_score[new_restaurant] = new_score
+    print_restaurant_score(restaurant_score)
+else:
+    print "Thank you and bye!"
+
+
 
